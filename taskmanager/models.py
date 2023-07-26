0094f3ffcd2d6870cf3352740d56ac1df6942c5b
clawrence00 <>
2023-07-26 20:46:16 +0000

    added stuff

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,7 +1,7 @@
 from taskmanager import db
 
 
-class Category(mydb.Model):
+class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)
